694d6103b2f0b7e9a77394fafe492535f6f291f2
Kristjan Lääne <xonproject@gmail.com>
2023-06-12 18:49:56 +0300

    change user details

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,36 @@
-import { getAuth } from "firebase/auth";
-import React, { useState, useEffect } from "react";
+import { getAuth, updateProfile, onAuthStateChanged } from "firebase/auth";
+import { Firestore, doc, updateDoc } from "firebase/firestore";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
 
 export const Profile = () => {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: null,
     email: null,
   });
   useEffect(() => {
-    console.log(auth.currentUser);
-    if (auth.currentUser) {
-      setFormData({
-        name: auth.currentUser.displayName,
-        email: auth.currentUser.email,
-      });
-    }
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setFormData({
+          name: user.displayName,
+          email: user.email,
+        });
+      } else {
+        setFormData({
+          name: null,
+          email: null,
+        });
+      }
+    });
+
+    return () => unsubscribe();
   }, [auth]);
 
+  console.log(auth.currentUser);
   const { name, email } = formData;
 
   const navigate = useNavigate();
@@ -25,6 +38,30 @@ export const Profile = () => {
   const onLogout = () => {
     auth.signOut();
     navigate("/");
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        const userRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      toast.error("Could not update profile details");
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
   return (
@@ -35,6 +72,42 @@ export const Profile = () => {
           Log out
         </button>
       </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
