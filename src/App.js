2df8c63d3a52c9c36de8f23c86f3bacc6ec0f673
Kristjan Lääne <xonproject@gmail.com>
2023-06-20 17:58:23 +0300

    contact landlord page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { PrivateRoute } from "./components/PrivateRoute.jsx";
 import { Category } from "./pages/Category";
 import { CreateListing } from "./pages/CreateListing.jsx";
 import { Listing } from "./pages/Listing";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         {<Navbar />}
       </Router>
